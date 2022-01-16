58615ed6d9009d3f66cba5fa2a3bfe5a6a5aa097
Davide Crestini <davide.crestini@theculturetrip.com>
2022-01-16 21:08:21 +0000

    chore: added some empty pages

---
+++ pages/admin/files/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const AdminFiles: NextPage = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-gray-600 font-bold">Files</h2>
+      <p className="text-gray-600">In this section you can manage your news.</p>
+    </div>
+  );
+};
+
+export default AdminFiles;
---
+++ pages/admin/inventory/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+
+const AdminInventory: NextPage = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-gray-600 font-bold">Resources</h2>
+      <p className="text-gray-600">
+        In this section you can manage your whole inventory.
+      </p>
+    </div>
+  );
+};
+
+export default AdminInventory;
